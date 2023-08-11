ce0c7416b0c0e2b935419407966e67c2ab37c97a
Patrik Johansson <patrik@ptjwebben.se>
2023-08-11 20:19:47 +0200

    Added support for config flow

---
+++ custom_components/tcl_tv_remote/config_flow.py
@@ -0,0 +1,34 @@
+"""Config flow TCL TV Remote integration."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries, exceptions
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+DOMAIN = "tcl_tv_remote"
+_LOGGER = logging.getLogger(__name__)
+
+DATA_SCHEMA = vol.Schema({})
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            try:
+                return self.async_create_entry(title="TCL Remote", data=user_input)
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
